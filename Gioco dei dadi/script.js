9e0cca75bf0a223b311fa9c0b630dc4f849f78fa
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-01 15:45:00 +0200

    Implementazione "js-dadi" completata

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -3,15 +3,25 @@
   Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
   Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-  Prima di partire a scrivere codice poniamoci qualche domanda:
-  Che ci sia un array da qualche parte?
-  Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
-  Consigli del giorno:
-  1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-  2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-  3. si ma noi cosa vogliamo fare?
-  4. torniamo a scrivere in italiano
-  5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-
 */
+
+// Generazione numeri random da 1 a 6
+const numGiocatore = Math.floor(Math.random() * 6) + 1;   /* Math.random() * 6) + 1; -> Restituisce un numero intero casuale da 1 a 6
+                                                             Math.floor arrotonda per difetto all'intero + vicino
+                                                          */
+const numComputer = Math.floor(Math.random() * 6) + 1; 
+
+// Stampa numeri generati
+console.log("Numero Giocatore: " + numGiocatore + " - Numero Computer: " + numComputer);
+
+if (numGiocatore === numComputer) {
+  console.log(numGiocatore + " = " + numComputer + " -> Nessun vincitore!");
+}
+
+else if (numGiocatore > numComputer) {
+  console.log(numGiocatore + " > " + numComputer + " -> Vince il giocatore!");
+}
+
+else {
+  console.log(numComputer + " > " + numGiocatore + " -> Vince il computer!");
+}
